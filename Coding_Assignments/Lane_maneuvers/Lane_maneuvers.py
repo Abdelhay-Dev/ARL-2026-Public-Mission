56f10ca9df2d4ccaec32f65194d2f2ca04222198
Abdelhay-Dev <mohamedmahmoudfff@gmail.com>
2025-09-01 12:02:51 +0300

    Lane_maneuvers.py

--- Coding_Assignments/Lane_maneuvers/Lane_maneuvers.py
+++ Coding_Assignments/Lane_maneuvers/Lane_maneuvers.py
@@ -1,11 +1,10 @@
 def analyze_drive(lanes: list[int]) -> tuple[int, int]:
-    """
-    Analyzes the driving behavior based on lane positions.
-    
-    Args:
-        lanes (list[int]): List of lane positions at each second (0, 1, or 2).
-    
-    Returns:
-        tuple[int, int]: (number of lane changes, number of dangerous maneuvers)
-    """
-    pass
+     count1 = count2 = 0
+     for i in range(len(lanes)-1):
+         diff = abs(lanes[i] - lanes[i + 1])
+         if diff==1:
+             count1 += 1
+         elif diff==2:
+             count2 += 1
+             count1 += 1
+     return count1, count2
